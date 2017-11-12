e1140459c48a2af9993068793848bceae1594c9a
Thomas Saunders <tasaunders@gmail.com>
2017-11-12 16:51:26 -0600

    Bump version: 0.1.3 → 0.1.5

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = dev_reqs
 
 setup(
     name='neo-python-rpc',
-    version='0.1.3',
+    version='0.1.5',
     description="A Python RPC Client for the NEO Blockchain",
     long_description=readme,
     author="Thomas Saunders",
