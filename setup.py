25861493b8617c67a1fb1e275a6846a67276ff3f
Erik van den Brink <git@erikvandenbrink.com>
2018-08-29 19:20:57 +0200

    Fix python 3.5 building

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@
 """The setup script."""
 
 from setuptools import setup, find_packages
+try:  # pip version >= 10.0
+    from pip._internal.req import parse_requirements
+    from pip._internal.download import PipSession
+except ImportError:  # pip version < 10.0
 from pip.req import parse_requirements
 from pip.download import PipSession
 
